7bc48933a386d36041a09d715dc62cc02308fba7
fancywhale23 <justintaoau@gmail.com>
2025-05-22 21:14:56 +1000

    style fixes in main

--- src/cli_main.cpp
+++ src/cli_main.cpp
@@ -23,21 +23,21 @@ int main(int argc, char *argv[]) {
     std::string format_str;
     int num_threads = 1;
 
+    static struct option long_opts[] = {
+        {"verbose", no_argument, 0, 'v'},
+        {"help", no_argument, 0, 'h'},
+        {"source", required_argument, 0, 's'},
+        {"dest", required_argument, 0, 'd'},
+        {"format", required_argument, 0, 'f'},
+        {"threads", required_argument, 0, 't'},
+        {"replace", no_argument, 0, 'r'},
+        {"move", no_argument, 0, 'm'},
+        {"preview", no_argument, 0, 'p'},
+        {0, 0, 0, 0}
+
+    };
     int opt_char;
     while (1) {
-        static struct option long_opts[] = {
-            {"verbose", no_argument, 0, 'v'},
-            {"help", no_argument, 0, 'h'},
-            {"source", required_argument, 0, 's'},
-            {"dest", required_argument, 0, 'd'},
-            {"format", required_argument, 0, 'f'},
-            {"threads", required_argument, 0, 't'},
-            {"replace", no_argument, 0, 'r'},
-            {"move", no_argument, 0, 'm'},
-            {"preview", no_argument, 0, 'p'},
-            {0, 0, 0, 0}
-
-        };
 
         int opt_index = 0;
         opt_char =
@@ -47,101 +47,101 @@ int main(int argc, char *argv[]) {
             break;
 
         switch (opt_char) {
-        case 0:
-            // if (long_opts[opt_index].flag != 0)
-            //     break;
-            break;
-        case 'h': {
-            std::cout << "Usage: utasort -f <format_str> -s <source_dir> "
-                         "[options]...\n";
-            std::cout << "Required:\n";
-            std::cout << "  -s, --source path\tPath to the source directory\n";
-            std::cout << "  -f, --format string\tFormat string for the "
-                         "directory structure\n";
-            std::cout << "Optional:\n";
-            std::cout << "  -d, --dest path\tPath to the destination "
-                         "directory;\n";
-            std::cout << "\t\t\tIf not specified, the source directory will be "
-                         "used\n";
-            std::cout << "  -t, --threads integer\tThe number of threads to "
-                         "use when copying files;\n";
-            std::cout << "\t\t\tThe default is 1, and it is recommended to use "
-                         "no more than 8\n";
-            std::cout << "  -r, --replace\t\tReplace files in the destination "
-                         "directory if there are collisions;\n";
-            std::cout << "\t\t\tIf not specified, existing files will not be "
-                         "overwritten\n";
-            std::cout << "  -p, --preview\t\tShow a list of files that would "
-                         "be moved/copied,;\n";
-            std::cout << "\t\t\tbut do not commit any changes to disk\n";
-            std::cout << "  -v, --verbose\t\tPrint more information, including "
-                         "the destination of all copied files\n";
-            std::cout << "Help:\n";
-            std::cout << "  -h, --help\t\tShow this help message and exit\n";
-            std::cout << "  -H, --format-help\tShow help for defining the "
-                         "format string and exit (doesn't work yet)\n";
+            case 0:
+                // if (long_opts[opt_index].flag != 0)
+                //     break;
+                break;
+            case 'h': {
+                std::cout << "Usage: utasort -f <format_str> -s <source_dir> "
+                    "[options]...\n";
+                std::cout << "Required:\n";
+                std::cout << "  -s, --source path\tPath to the source directory\n";
+                std::cout << "  -f, --format string\tFormat string for the "
+                    "directory structure\n";
+                std::cout << "Optional:\n";
+                std::cout << "  -d, --dest path\tPath to the destination "
+                    "directory;\n";
+                std::cout << "\t\t\tIf not specified, the source directory will be "
+                    "used\n";
+                std::cout << "  -t, --threads integer\tThe number of threads to "
+                    "use when copying files;\n";
+                std::cout << "\t\t\tThe default is 1, and it is recommended to use "
+                    "no more than 8\n";
+                std::cout << "  -r, --replace\t\tReplace files in the destination "
+                    "directory if there are collisions;\n";
+                std::cout << "\t\t\tIf not specified, existing files will not be "
+                    "overwritten\n";
+                std::cout << "  -p, --preview\t\tShow a list of files that would "
+                    "be moved/copied,;\n";
+                std::cout << "\t\t\tbut do not commit any changes to disk\n";
+                std::cout << "  -v, --verbose\t\tPrint more information, including "
+                    "the destination of all copied files\n";
+                std::cout << "Help:\n";
+                std::cout << "  -h, --help\t\tShow this help message and exit\n";
+                std::cout << "  -H, --format-help\tShow help for defining the "
+                    "format string and exit (doesn't work yet)\n";
 
-            return 0;
-        }
-        case 'v':
-            verbose_flag = 1;
-            break;
-        case 's': {
-            src_dir = optarg;
-            while (src_dir.back() == '/') {
-                src_dir.pop_back();
+                return 0;
             }
-            src_dir = src_dir + '/';
+            case 'v':
+                verbose_flag = 1;
+                break;
+            case 's': {
+                src_dir = optarg;
+                while (src_dir.back() == '/') {
+                    src_dir.pop_back();
+                }
+                src_dir = src_dir + '/';
 
-            if (!fs::exists(src_dir) || !fs::is_directory(src_dir)) {
-                fprintf(stderr,
-                        "Source directory '%s' does not exist or is not a "
-                        "directory\n",
-                        optarg);
-                return 1;
-            }
-            break;
-        }
-        case 'd': {
-            dest_dir = optarg;
-            while (dest_dir.back() == '/') {
-                dest_dir.pop_back();
-            }
-            dest_dir = dest_dir + '/';
-
-            if (fs::exists(dest_dir)) {
-                if (!fs::is_directory(dest_dir)) {
+                if (!fs::exists(src_dir) || !fs::is_directory(src_dir)) {
                     fprintf(stderr,
-                            "Destination directory '%s' is not a directory\n",
+                            "Source directory '%s' does not exist or is not a "
+                            "directory\n",
                             optarg);
                     return 1;
                 }
-            } else {
-                fprintf(stderr, "Destination directory '%s' does not exist",
-                        optarg);
-                return 1;
+                break;
             }
-            break;
-        }
-        case 't':
-            num_threads = atoi(optarg);
-            break;
-        case 'f':
-            format_str = optarg;
-            break;
-        case 'r':
-            replace_flag = 1;
-            break;
-        case 'm':
-            move_flag = 1;
-            break;
-        case 'p':
-            preview_flag = 1;
-            break;
-        case '?':
-            break;
-        default:
-            abort();
+            case 'd': {
+                dest_dir = optarg;
+                while (dest_dir.back() == '/') {
+                    dest_dir.pop_back();
+                }
+                dest_dir = dest_dir + '/';
+
+                if (fs::exists(dest_dir)) {
+                    if (!fs::is_directory(dest_dir)) {
+                        fprintf(stderr,
+                                "Destination directory '%s' is not a directory\n",
+                                optarg);
+                        return 1;
+                    }
+                } else {
+                    fprintf(stderr, "Destination directory '%s' does not exist",
+                            optarg);
+                    return 1;
+                }
+                break;
+            }
+            case 't':
+                num_threads = atoi(optarg);
+                break;
+            case 'f':
+                format_str = optarg;
+                break;
+            case 'r':
+                replace_flag = 1;
+                break;
+            case 'm':
+                move_flag = 1;
+                break;
+            case 'p':
+                preview_flag = 1;
+                break;
+            case '?':
+                break;
+            default:
+                abort();
         }
     }
 
